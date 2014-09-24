23567518df2526cf66cb87bb0aaea7a4247c298a
Ben Mathes <mathes.ben@gmail.com>
2014-09-24 09:17:24 -0700

    wooooo

--- messing.hs
+++ messing.hs
@@ -1,16 +1,2 @@
-square x = (x) * (x)
+f x = if x > 100 then x*x else x*5
 
-fizzbuzz :: (Integral a, Show a) => a -> [[Char]]
-fizzbuzz max = [ 
-  if mod x 15 == 0 
-    then "fizzbuzz"
-    else (
-      if mod x 5 == 0 
-        then  "buzz" 
-        else (
-          if mod x 3 == 0 
-          then "fizz" 
-          else show x
-        )
-     )
-    | x <- [1..max] ]
